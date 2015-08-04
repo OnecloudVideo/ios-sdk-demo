434e1237589f91841b2e2b5bdd9ced2b27db100a
OnecloudVideo <open.video@onecloud.cn>
2015-08-04 09:24:03 +0800

    remove test app secret and key

---
+++ AppDemo/UsageIPViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  UsageIPTViewCell.swift
+//  AppDemo
+//
+//  Created by kinghai on 7/29/15.
+//  Copyright (c) 2015 onecloud.inc. All rights reserved.
+//
+
+import UIKit
+
+class UsageIPViewCell: UsageBaseViewCell {
+
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var ipLbl: UILabel!
+   
+    override func updateUsage(usage: Usage<UInt>) {
+        dateLbl.text = AppUtil.stringFromDate(usage.startAt)
+        ipLbl.text = "\(usage.usage)"
+    }
+}
